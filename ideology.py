441dc3f0fb023876299c299e97747ee0cbdf459c
ravenusmc <mcuddy77@gmail.com>
2017-02-16 14:36:16 -0500

    worked more on ideology pages

---
+++ ideology.py
@@ -0,0 +1,6 @@
+#This file will hold the class for the ideology information.
+
+class Ideology():
+
+    def right_wing_info():
+        self.__data = pd.read_csv('')
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 
 #importing all files
 from flask import Flask, render_template, request
+from ideology import *
 
 #Setting up flask
 app = Flask(__name__)
@@ -13,7 +14,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title='Home Page')
 
-#Code for pages dealing with ideology
+#Code for pages dealing with ideology pages
 @app.route('/ideology')
 def idea():
     return render_template('idea.html', title='Ideology Page')
@@ -21,6 +22,14 @@ def idea():
 @app.route('/right_wing')
 def right():
     return render_template('right.html', title='Right Wing Terrorism Info')
+
+@app.route('/left_wing')
+def left():
+    return render_template('left.html', title='Left Wing Terrorism Info')
+
+@app.route('/islamic')
+def ideology():
+    return render_template('islamic.html', title='Islamic Terrorism Info')
 
 #This line will actually run the app.
 app.run(debug=True)
